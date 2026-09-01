3cc8695412b9fd03ceac36094a55073b2252586a
albertgmi <gmitrzak.albert@gmail.com>
2026-09-01 13:44:57 +0200

    menu logo changed

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -122,7 +122,7 @@ export class AppMenu implements OnInit {
               { label: 'Active Students Reports', icon: 'pi pi-chart-bar', routerLink: ['/system/active-students-reports'] },
               { label: 'Check Essays',    icon: 'pi pi-file-edit',    routerLink: ['/lesson/check-essays'] },
               { label: 'Live Essay Room', icon: 'pi pi-comments', routerLink: ['/live-essay-room'] },
-              { label: 'Live Sentence Room', icon: 'pi pi-comments', routerLink: ['/live-sentence-room'] }
+              { label: 'Live Sentence Room', icon: 'pi pi-sync', routerLink: ['/live-sentence-room'] }
             ]
           },
           {
@@ -172,7 +172,7 @@ export class AppMenu implements OnInit {
           { label: 'Assignments', icon: 'pi pi-file', routerLink: ['/assignments'] },
           { label: 'My Essays', icon: 'pi pi-file-edit', routerLink: ['/my-essays'] },
           { label: 'Live Essay Room', icon: 'pi pi-comments', routerLink: ['/live-essay-room'] },
-          { label: 'Live Sentence Room', icon: 'pi pi-comments', routerLink: ['/live-sentence-room'] },
+          { label: 'Live Sentence Room', icon: 'pi pi-sync', routerLink: ['/live-sentence-room'] },
           { label: 'Movie Theater', icon: 'pi pi-video', routerLink: ['/weekly-movies'] }
         ]
       },
